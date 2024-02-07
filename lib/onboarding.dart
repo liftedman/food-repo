243ca945b91a1ef0e0f7675e7d0b3b3412feb853
liftedman <gbeminiyiakinfala@gmail.com>
2024-02-07 11:46:44 +0100

    initialize project

---
+++ lib/onboarding.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+// import 'package:my_food_app/homepage.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  final controller = PageController();
+  bool isLastPage = false;
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: controller,
+        onPageChanged: (index) {
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
